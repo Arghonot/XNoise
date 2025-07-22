7ef63f8ecf3760b10407005c2f45b219a0f283c4
Arghonaut <lo.rivemale@gmail.com>
2025-07-22 11:04:49 +0700

    Made the turbulence use a seed on its perlin modules that can be set by the user.

--- LibnoiseWrapers/Operators/TurbulenceTransformer.cs
+++ LibnoiseWrapers/Operators/TurbulenceTransformer.cs
@@ -20,7 +20,13 @@ namespace XNoise
 
         #endregion
 
+        private int _perlinsSeed = 0;
+
         public TurbulenceTransformer(double power, ModuleBase input) : base(power, input) { }
+        public TurbulenceTransformer(double power, ModuleBase input, int Seed) : base(power, input)
+        {
+            _perlinsSeed = Seed;
+        }
 
         public double GetValueCPU(double x, double y, double z) => GetValue(x, y, z);
 
@@ -31,13 +37,18 @@ namespace XNoise
             var tmpTurbulence = datas.displacementMap;
             Vector3 tmpOrigin = datas.origin;
 
+            var PerlinModule = new PerlinGenerator();
+
             materialGPU.SetTexture("_OriginalDisplacementMap", datas.displacementMap);
             datas.origin = new Vector3((float)X0, (float)Y0, (float)Z0);
-            materialGPU.SetTexture("_PerlinA", new PerlinGenerator().GetValueGPU(datas));
+            PerlinModule.Seed = _perlinsSeed;
+            materialGPU.SetTexture("_PerlinA", PerlinModule.GetValueGPU(datas));
             datas.origin = new Vector3((float)X1, (float)Y1, (float)Z1);
-            materialGPU.SetTexture("_PerlinB", new PerlinGenerator().GetValueGPU(datas));
+            PerlinModule.Seed = _perlinsSeed - 1;
+            materialGPU.SetTexture("_PerlinB", PerlinModule.GetValueGPU(datas));
             datas.origin = new Vector3((float)X2, (float)Y2, (float)Z2);
-            materialGPU.SetTexture("_PerlinC", new PerlinGenerator().GetValueGPU(datas));
+            PerlinModule.Seed = _perlinsSeed + 1;
+            materialGPU.SetTexture("_PerlinC", PerlinModule.GetValueGPU(datas));
             materialGPU.SetFloat("_Power", (float)Power);
             datas.origin = tmpOrigin;
 
--- Nodes/Operator/TurbulenceNode.cs
+++ Nodes/Operator/TurbulenceNode.cs
@@ -9,13 +9,14 @@ namespace XNoise
     {
         [Input(ShowBackingValue.Always, ConnectionType.Override, TypeConstraint.InheritedAny)]
         public ModuleBase Input;
-
         [Input(ShowBackingValue.Always, ConnectionType.Override, TypeConstraint.Strict)]
         public double Power;
+        [Input(ShowBackingValue.Always, ConnectionType.Override, TypeConstraint.Strict)]
+        public int Seed;
 
         public override object Run()
         {
-            return new TurbulenceTransformer(GetInputValue<double>("Power", this.Power), GetInputValue<ModuleBase>("Input", this.Input));
+            return new TurbulenceTransformer(GetInputValue<double>("Power", this.Power), GetInputValue<ModuleBase>("Input", this.Input), GetInputValue<int>("Seed", this.Seed));
         }
     }
 }
